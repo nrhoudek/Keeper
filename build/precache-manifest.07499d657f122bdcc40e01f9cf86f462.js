a1266024f827edabf18c3019f0616edc18a0ec5e
Nick Houdek <nickhoudek@gmail.com>
2021-02-10 22:47:30 -0700

    Create local server for testing

---
+++ build/precache-manifest.07499d657f122bdcc40e01f9cf86f462.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e62ad2dba32544cb729b98683f302a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9728459f1e72872d22c3",
+    "url": "/static/js/2.eb838042.chunk.js"
+  },
+  {
+    "revision": "5d775c9854340d33484c",
+    "url": "/static/js/main.6228669f.chunk.js"
+  },
+  {
+    "revision": "05c790a9a60e4d4b4be2",
+    "url": "/static/js/runtime-main.a8757f30.js"
+  }
+]);
